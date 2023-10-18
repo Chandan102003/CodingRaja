f2e6bdcf706af572cfcddf20937d485aa9a49c50
Chandan102003 <126407312+Chandan102003@users.noreply.github.com>
2023-10-18 12:22:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Select all the elements in the HTML page
-// and assign them to a variable
+
 let now_playing = document.querySelector(".now-playing");
 let track_art = document.querySelector(".track-art");
 let track_name = document.querySelector(".track-name");
@@ -14,15 +13,14 @@ let volume_slider = document.querySelector(".volume_slider");
 let curr_time = document.querySelector(".current-time");
 let total_duration = document.querySelector(".total-duration");
 
-// Specify globally used values
+
 let track_index = 0;
 let isPlaying = false;
 let updateTimer;
 
-// Create the audio element for the player
+
 let curr_track = document.createElement('audio');
 
-// Define the list of tracks that have to be played
 let track_list = [
 {
 	name: "Night Owl",
@@ -44,15 +42,15 @@ let track_list = [
 },
 ];
 function loadTrack(track_index) {
-// Clear the previous seek timer
+
 clearInterval(updateTimer);
 resetValues();
 
-// Load a new track
+
 curr_track.src = track_list[track_index].path;
 curr_track.load();
 
-// Update details of the track
+
 track_art.style.backgroundImage = 
 	"url(" + track_list[track_index].image + ")";
 track_name.textContent = track_list[track_index].name;
@@ -60,15 +58,10 @@ track_artist.textContent = track_list[track_index].artist;
 now_playing.textContent = 
 	"PLAYING " + (track_index + 1) + " OF " + track_list.length;
 
-// Set an interval of 1000 milliseconds
-// for updating the seek slider
 updateTimer = setInterval(seekUpdate, 1000);
 
-// Move to the next track if the current finishes playing
-// using the 'ended' event
 curr_track.addEventListener("ended", nextTrack);
 
-// Apply a random background color
 random_bg_color();
 }
 
